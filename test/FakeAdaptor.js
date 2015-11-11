9d4208b20b668f116659e44d25611987c289b628
Stuart Corbishley <corbish@gmail.com>
2015-11-11 17:24:37 +0200

    JSON Path wrapper and Fake Adaptor for tests

--- test/FakeAdaptor.js
+++ test/FakeAdaptor.js
@@ -1,19 +1,36 @@
-const sObjectStub = {
-  upsert: function(fields, extId) {
-    console.log(fields, extId);
-  }
+import { curry, merge, mapValues } from 'lodash-fp';
+import JSONPath from 'JSONPath';
+
+function steps(...operations) {
+  return operations;
 }
 
-const connection = {
-  sobject: function(object) {
-    return sObjectStub;
-  }
+function expandReferences(state, attrs) {
+  return mapValues(function(value) {
+    return typeof value == 'function' ? value(state) : value;
+  })(attrs); 
 }
 
-export default class FakeAdaptor {
-  static execute(operations) {
-    operations.map(function(operation) {
-      return operation(connection);
-    })
-  }
+const create = curry(function(sObject, fields, state) {
+  return { sObject, fields: expandReferences(state, fields) };
+});
+
+function reference(pos) {
+  return pos;
 }
+
+const sourceValue = curry(function(path, {data}) {
+  return JSONPath.eval(data, path)[0];
+})
+
+const source = curry(function(path, {data}) {
+  return JSONPath.eval(data, path);
+})
+
+const map = curry(function(path, operation, state) {
+  return source(path,state).map(function(data) {
+    return operation(merge({ data }, state));
+  })
+})
+
+export { create, reference, steps, source, sourceValue, map }
---
+++ test/json_reference.test.js
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import { create, source, sourceValue, steps, map } from './FakeAdaptor';
+import testData from './testData';
+
+steps(
+  map('$.store.book', 
+    create('Book', 'sp_id__c', {
+      title: source("$.title")
+    })
+  )
+)
+
+describe("JSON References", () => {
+
+  describe("One to one", () => {
+    it("references a given path", () => {
+      let value = sourceValue("$.store.bicycle.color", {data: testData});
+      expect(value).to.eql("red");
+    })
+  })
+
+  describe("Use cases", () => {
+
+    const state = {data: testData};
+
+    it("can produce a one to one", () => {
+      let obj = create("myObject", {
+        bicycle: sourceValue("$.store.bicycle.color", state)
+      }, state)
+
+      expect(obj).to.eql({ sObject: "myObject", fields: { bicycle: "red" } })
+    });
+
+    it("can produce a one to one from an array", () => {
+      let obj = map('$.store.book[*]', 
+        create('Book', {
+          title: sourceValue("$.title")
+        })
+        , state)
+
+      expect(obj).to.eql([
+        {
+          "fields": {
+            "title": "Sayings of the Century"
+          },
+          "sObject": "Book"
+        },
+        {
+          "fields": {
+            "title": "Sword of Honour"
+          },
+          "sObject": "Book"
+        },
+        {
+          "fields": {
+            "title": "Moby Dick"
+          },
+          "sObject": "Book"
+        },
+        {
+          "fields": {
+            "title": "The Lord of the Rings"
+          },
+          "sObject": "Book"
+        }
+      ])
+      
+    });
+  })
+  
+})
+
+
