901a404360e25739e6d5411cb9f48af391238f7d
Taylor Downs <downs.taylor@gmail.com>
2016-02-29 15:08:53 +0200

    add relationship function

--- src/sourceHelpers.js
+++ src/sourceHelpers.js
@@ -16,3 +16,25 @@ export function lookup(relationshipName, externalId, path) {
     return { [externalId]: sourceValue(path)(state) }
   })
 }
+
+
+/**
+ * Adds a lookup or 'dome insert' to a record.
+ * @example <caption>Data Sourced Value</caption>
+ * relationship("relationship_name__r", "externalID on related object", dataSource("path"))
+ * @example <caption>Fixed Value</caption>
+ * relationship("relationship_name__r", "externalID on related object", "hello world") 
+ * @constructor
+ * @param {string} relationshipName - `__r` relationship field on the record.
+ * @param {string} externalID - Salesforce ExternalID field.
+ * @param {<DataSource>|string} dataSource - resolvable source.
+ * @returns {<Field>}
+ */
+export function relationship(relationshipName, externalId, dataSource) {
+  return field(relationshipName, (state) => {
+    if (typeof dataSource == 'function') {
+      return { [externalId]: dataSource(state) }
+    }
+    return { [externalId]: dataSource }
+  })
+}
