83ef856820942c1bc34f69e3ed7b5da357a8caab
Stuart Corbishley <corbish@gmail.com>
2016-04-12 11:22:29 +0200

    Forgot to export the new helpers from common

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -156,5 +156,5 @@ export { lookup, relationship } from './sourceHelpers';
 export {
   each, join, fields, field, source, sourceValue, map, combine,
   merge, dataPath, dataValue, referencePath, lastReferenceValue,
-  index, beta
+  index, beta, toArray, arrayToString
 } from 'language-common';
