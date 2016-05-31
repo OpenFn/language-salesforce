13262b767bf0113346be5401ed5ca0dfb0ae0bcf
Stuart Corbishley <corbish@gmail.com>
2016-05-31 14:50:20 +0200

    Import members, not default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Adaptor from './Adaptor';
-import FakeAdaptor from './FakeAdaptor';
+import * as Adaptor from './Adaptor';
+import * as FakeAdaptor from './FakeAdaptor';
 
 export default Adaptor;
 export { FakeAdaptor, Adaptor };
