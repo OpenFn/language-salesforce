46526c099ea4997679bab5d9fe11acd858326c03
taylordowns2000 <downs.taylor@gmail.com>
2016-09-20 11:42:52 +0200

    add alterState to salesforce

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -158,5 +158,5 @@ export { lookup, relationship } from './sourceHelpers';
 export {
   each, join, fields, field, source, sourceValue, map, combine,
   merge, dataPath, dataValue, referencePath, lastReferenceValue,
-  index, beta, toArray, arrayToString
+  index, beta, toArray, arrayToString, alterState
 } from 'language-common';
