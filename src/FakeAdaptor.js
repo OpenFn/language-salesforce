7dcdcb43efcb1db7ea2c1373cf4bf9de4843c30d
Santiago Adolfo Gutierrez Akel <sgutierrezakel@gmail.com>
2017-11-30 15:57:55 -0300

    proper documentation for FakeAdaptor

--- src/FakeAdaptor.js
+++ src/FakeAdaptor.js
@@ -1,20 +1,57 @@
+/** @module FakeAdaptor */
 import { execute as commonExecute } from 'language-common';
 import { curry, mapValues, flatten } from 'lodash-fp';
 
-/** @module FakeAdaptor */
-
-function steps(...operations) {
+/**
+ * Flattens an array of operations.
+ * @public
+ * @example
+ *  steps(
+ *    createIf(params),
+ *    update(params)
+ *  )
+ * @function
+ * @returns {Array}
+ */
+export function steps(...operations) {
   return flatten(operations);
 }
 
-// TODO: use the one from language-common
-function expandReferences(attrs, state) {
+/**
+ * Expands references.
+ * @example
+ *  expandReferences(
+ *    state,
+ *    {
+ *      attr1: "foo",
+ *      attr2: "bar"
+ *    }
+ *  )
+ * @function
+ * @param {Object} attrs - Field attributes for the new object.
+ * @param {State} state - Runtime state.
+ * @returns {State}
+ */
+export function expandReferences(attrs, state) {
   return mapValues(function(value) {
     return typeof value == 'function' ? value(state) : value;
   })(attrs);
 }
 
-function create(sObject, fields) {
+/**
+ * Create a new object.
+ * @public
+ * @example
+ *  create('obj_name', {
+ *    attr1: "foo",
+ *    attr2: "bar"
+ *  })
+ * @function
+ * @param {String} sObject - API name of the sObject.
+ * @param {Object} fields - Field attributes for the new object.
+ * @returns {Operation}
+ */
+export function create(sObject, fields) {
 
   return (state) => {
 
@@ -33,7 +70,20 @@ function create(sObject, fields) {
   }
 }
 
-function update(sObject, fields) {
+/**
+ * Update an object.
+ * @public
+ * @example
+ *  update('obj_name', {
+ *    attr1: "foo",
+ *    attr2: "bar"
+ *  })
+ * @function
+ * @param {String} sObject - API name of the sObject.
+ * @param {Object} fields - Field attributes for the new object.
+ * @returns {Operation}
+ */
+export function update(sObject, fields) {
 
   return (state) => {
 
@@ -52,7 +102,21 @@ function update(sObject, fields) {
   }
 }
 
-function upsert(sObject, externalId, fields) {
+/**
+ * Upsert an object.
+ * @public
+ * @example
+ *  upsert('obj_name', 'ext_id', {
+ *    attr1: "foo",
+ *    attr2: "bar"
+ *  })
+ * @function
+ * @param {String} sObject - API name of the sObject.
+ * @param {String} externalId - ID.
+ * @param {Object} fields - Field attributes for the new object.
+ * @returns {Operation}
+ */
+export function upsert(sObject, externalId, fields) {
 
   return (state) => {
 
@@ -71,12 +135,27 @@ function upsert(sObject, externalId, fields) {
   }
 }
 
-
-const reference = curry(function(position, {references}) {
+/**
+ * Get a reference ID by an index.
+ * @public
+ * @example
+ *  reference(0)
+ * @function
+ * @param {number} position - Position for references array.
+ * @param {State} state - Array of references.
+ * @returns {State}
+ */
+export const reference = curry(function(position, state) {
+  const { references } = state;
   return references[position].id;
 })
 
-
+/**
+ * Executes an operation.
+ * @function
+ * @param {Operation} operations - Operations
+ * @returns {State}
+ */
 function execute(...operations) {
 
   const initialState = {
@@ -110,16 +189,6 @@ function execute(...operations) {
     })
   };
 
-}
-
-
-export {
-  create,
-  execute,
-  reference,
-  steps,
-  update,
-  upsert
 }
 
 export { lookup, relationship } from './sourceHelpers';
